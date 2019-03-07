992cac3d913bfbf66ca067a11be7a5544d369798
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-07 14:03:51 -0400

    Removing boilerplate of layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,6 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import { ThemeProvider } from 'styled-components'
 
-import Header from "./header"
-import "./layout.css"
-
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -20,22 +17,7 @@ const Layout = ({ children }) => (
     render={data => (
       <ThemeProvider theme={{}}>
         <>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
-          </div>
+          {children}
         </>
       </ThemeProvider>
     )}
